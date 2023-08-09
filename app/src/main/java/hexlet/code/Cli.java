e41a8d69b4ec6d560aac08636ab493f7949387f3
ungrome <ungarov.roman@gmail.com>
2023-08-09 20:52:30 +0300

    add Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String username = scanner.next();
+    System.out.println("Hello, " + username + "!");
+    scanner.close();
+}
+}
