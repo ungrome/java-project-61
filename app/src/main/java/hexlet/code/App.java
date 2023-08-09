0c7e5685fa72b1385e6a07bad84b7e64bc4c2caf
ungrome <ungarov.roman@gmail.com>
2023-08-09 19:33:38 +0300

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,7 @@
+package hexlet.code;
 class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Welcome to the Brain Games!");
         }
 }
