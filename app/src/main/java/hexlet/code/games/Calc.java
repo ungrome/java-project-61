2ab3bbf4dd9b3bef71303286112c6a9094a368f1
ungrome <ungarov.roman@gmail.com>
2023-08-16 19:32:30 +0300

    add code in Calc.java (step 6)

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,7 +1,36 @@
 package hexlet.code.games;
 
 public class Calc {
-   // public static start(String name) {
+    public static String gameCode() {
+        int result;
+        int number1 = (int) (Math.random() * 10);
+        int number2 = (int) (Math.random() * 10);
+        System.out.print("Question: ");
+        result = operation(number1, number2);
+        return String.valueOf(result);
+    }
 
+    public static int operation(int num1, int num2) {
+        String[] operators = {"+", "-", "*" };
+        int i = (int) (Math.random() * 3);
+        String operator = operators[i];
+        switch (operator) {
+            case "+" :
+                System.out.println(num1 + " + " + num2);
+                return (num1 + num2);
+            case "-" :
+                System.out.println(num1 + " - " + num2);
+                return (num1 - num2);
+            case "*" :
+                System.out.println(num1 + " * " + num2);
+                return (num1 * num2);
+            default:
+        }
+        return 0;
+    }
+
+    public static String gameTask() {
+        return "What is the result of the expression?";
+    }
  //   }
 }
