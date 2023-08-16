7356eb214e7e7bfa6f2643115d917775bc1d0f84
ungrome <ungarov.roman@gmail.com>
2023-08-16 19:35:01 +0300

    modify Engine.java (add calcGame in case "3")

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,31 +1,41 @@
 package hexlet.code;
 
 import java.util.Scanner;
-import hexlet.code.Cli;
+import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 
 public class Engine {
     static final int COUNT_CORRECT_ANSWER_TO_WIN = 3;
     static final int NUMBER_OF_ROUNDS = 3;
 
-    public static void gameProcess() {
+    public static void gameProcess(String userChoice) {
         Scanner s = new Scanner(System.in);
         boolean result = true;
         int countOfCorrectAnswer = 0;
         String correctAnswer = "";
         String lastAnswer = "";
         String lastCorrectAnswer = "";
-        String usersAnswer = "";
-        while ((countOfCorrectAnswer < NUMBER_OF_ROUNDS) && (result == true)) {
-            correctAnswer = Even.gameCode();
+        String usersAnswer;
+        while ((countOfCorrectAnswer < NUMBER_OF_ROUNDS) && result) {
+            switch (userChoice) {
+                case "2":
+                    correctAnswer = Even.gameCode();
+                    break;
+                case "3":
+                    correctAnswer = Calc.gameCode();
+                    break;
+                default:
+            }
             usersAnswer = s.next();
 
             result = checkAnswer(correctAnswer, usersAnswer);
-            if (result == false) {
+            if (!result) {
                 lastAnswer = usersAnswer;
                 lastCorrectAnswer = correctAnswer;
                 break;
-            } else countOfCorrectAnswer++;
+            } else {
+                countOfCorrectAnswer++;
+            }
         }
         resultAnnouncement(Cli.getUserName(), countOfCorrectAnswer, lastCorrectAnswer, lastAnswer);
     }
@@ -38,7 +48,6 @@ public class Engine {
             return false;
         }
     }
-
     public static void resultAnnouncement(String userName, int countOfCorrectAnswer,
                                           String correctAnswer, String usersAnswer) {
         if (countOfCorrectAnswer == COUNT_CORRECT_ANSWER_TO_WIN) {
@@ -50,15 +59,3 @@ public class Engine {
         }
     }
 }
-//    public static boolean textToBoolean(String answer) {
-//        return answer.equals("yes");
-//    }
-//
-//    public static String booleanToText(boolean answer) {
-//        return answer? "yes": "no";
-//    }
-
-//    public static int randomGenerator() {
-//        return String.valueOf((int) (Math.random() * 100));
-//    }
-//}
