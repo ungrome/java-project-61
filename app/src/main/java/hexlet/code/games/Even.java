4cb0a5d55d9d8209d8bfb8895e3c1054bb473a26
ungrome <ungarov.roman@gmail.com>
2023-08-16 19:35:25 +0300

    fix linter errors Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,19 +1,13 @@
 package hexlet.code.games;
 
-import hexlet.code.Engine;
-
 public class Even {
     public static String gameCode() {
-            int number = (int) (Math.random() * 100);
-            System.out.print("Question: ");
-            System.out.println(number);
-            String correctAnswer = "";
-            correctAnswer = isEven(number)? "yes": "no";
-            return correctAnswer;
-            //            if (!input.equals("yes") && !input.equals("no")) {
-//                System.out.println("Error! Please, check your answer, " + Cli.getUserName() + ". "
-//                        + "It should be 'yes', or 'no'");
-//                continue;
+        int number = (int) (Math.random() * 100);
+        System.out.print("Question: ");
+        System.out.println(number);
+        String correctAnswer;
+        correctAnswer = isEven(number) ? "yes" : "no";
+        return correctAnswer;
     }
 
     public static boolean isEven(int number) {
